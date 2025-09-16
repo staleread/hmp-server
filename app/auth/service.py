0500260b9362a3b1392314bcd8ac0d40d26ba5c1
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2025-09-16 23:20:22 +0300

    refactor: fix naming and Base64 decoding call

--- app/auth/service.py
+++ app/auth/service.py
@@ -13,12 +13,12 @@ def verify_signed_challenge(
     *, signature_b64: str, challenge_b64: str, public_key_b64: str
 ) -> bool:
     try:
-        signature = base64.b64decode(signature_b64)
-        challenge = base64.b64decode(challenge_b64)
+        signature_bytes = base64.b64decode(signature_b64)
+        challenge_bytes = base64.b64decode(challenge_b64)
+        public_key_bytes = base64.b64decode(public_key_b64)
 
-        public_key_bytes = base64.b64decode(public_key_b64.encode("utf-8"))
         public_key = ed25519.Ed25519PublicKey.from_public_bytes(public_key_bytes)
-        public_key.verify(signature, challenge)
+        public_key.verify(signature_bytes, challenge_bytes)
 
         return True
     except InvalidSignature:
