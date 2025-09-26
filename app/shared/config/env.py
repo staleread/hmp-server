5b576fa10bc061ec98f1e62f747f40dcacb59b87
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2025-09-27 01:11:53 +0300

    Fix postgres DB name env

--- app/shared/config/env.py
+++ app/shared/config/env.py
@@ -13,12 +13,12 @@ class EnvSettings(BaseSettings):
     postgres_password: str
     postgres_host: str
     postgres_port: str = "5432"
-    postgres_database: str
+    postgres_db: str
 
     @computed_field  # type: ignore
     @property
     def postgres_url(self) -> str:
-        return f"postgresql+psycopg2://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database}"
+        return f"postgresql+psycopg2://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
 
 
 env_settings = EnvSettings()  # type: ignore
